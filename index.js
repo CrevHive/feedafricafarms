601fa3cd8a049ea067d44d33f98a7dbb4c59be61
Adetuyi Tolu <tolu.adetuyi@gmail.com>
2018-08-11 18:08:19 +0100

    update port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');//express is a framework for node.js web application
 const app = express(); //this is an instance of the express class
-const port = process.env.PORT || 3000; //connection port to be used
+const port = process.env.PORT || 8080; //connection port to be used
 var path = require('path');
-app.use(express.static(__dirname + '/public'));
+
+//app.use(express.static(path.join(__dirname + '/public')));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
