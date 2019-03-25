de5dc0f78f18603c499d6ba817e63a12b9f3e290
adetuyiTolu <tolu.adetuyi@gmail.com>
2019-03-25 13:18:49 +0100

    add email validation to form

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -2,7 +2,6 @@
   "use strict";
 
   $("#submit").click(function(e) {
-    
     e.preventDefault();
 
     var name = $("#name").val();
@@ -12,8 +11,21 @@
     var phone = $("#phone").val();
     var desc = $("#text").val();
 
-    var message =
-      "Hi Feed Africa Farms, \nBelow is a new request from " + name;
+    if (name.length < 3) {
+      respond("Fill in your correct full Name", "error");
+      return;
+    } else if (!isValidEmailAddress(email)) {
+      respond("Fill in valid email address", "error");
+      return;
+    } else if (phone.length < 6) {
+      respond("Fill in your Phone number", "error");
+      return;
+    } else if (desc.length < 10) {
+      respond("Fill in your request in full", "error");
+      return;
+    }
+
+    var message = "Hi Feed Africa Farms, \nBelow is a new request from " + name;
     message += "\n\nPartner Type: " + type;
     message += "\nSubject: " + subject;
     message += "\nPhone Number: " + phone;
@@ -23,26 +35,33 @@
     $.ajax({
       url: "api/feedback/",
       type: "POST",
-      data: { message: message},
+      data: { message: message },
       success: data => {
         respond(
           "Thanks for reaching out.\nYour request have been received, You will be contacted shortly.",
-          'success'
+          "success"
         );
       },
       error: err => {
-        respond("Couldn't process your request, please try again",
-        'error');
+        respond("Couldn't process your request, please try again", "error");
       }
-    }); 
+    });
   });
 })(jQuery);
 
-respond = (data,type) => {
+respond = (data, type) => {
   Swal.fire({
-    title: 'Request Sent!',
+    title: "Request Sent!",
     text: data,
     type: type,
-    confirmButtonText: 'Ok'
-  })
+    confirmButtonText: "Ok"
+  });
+
+  isValidEmailAddress = emailAddress => {
+    var pattern = new RegExp(
+      /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i
+    );
+    return pattern.test(emailAddress);
+  };
+  
 };
